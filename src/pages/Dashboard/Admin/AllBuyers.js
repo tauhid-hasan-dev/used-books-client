8a3731dafb6a387121dc709e5eb228fa37f1bcee
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-11-26 11:01:26 +0300

    data loaded for all buyers and all seller for admin route

--- src/pages/Dashboard/Admin/AllBuyers.js
+++ src/pages/Dashboard/Admin/AllBuyers.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../context/AuthProvider';
+import Loading from '../../../Loader/Loading';
 
 const AllBuyers = () => {
+
+    const { data: buyers = [], isLoading } = useQuery({
+        queryKey: ['buyers',],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/users?role=buyer`)
+            const data = await res.json();
+            return data;
+        }
+    })
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
     return (
         <div>
-            <p>All buyers</p>
+            <div className="overflow-x-auto ">
+                <table className="table w-full ">
+                    <thead className='bg-banner'>
+                        <tr>
+                            <th className='bg-nav-color text-white'>No</th>
+                            <th className='bg-nav-color text-white'>Name</th>
+                            <th className='bg-nav-color text-white'>Email</th>
+                            <th className='bg-nav-color text-white'>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody className='bg-banner'>
+                        {
+                            buyers.map((buyer, i) => <tr key={buyer._id}>
+                                <th className='bg-category text-white'>{i + 1}</th>
+                                <td className='bg-category text-white'>{buyer?.name}</td>
+                                <td className='bg-category text-white'>{buyer?.email}</td>
+                                <td className='bg-category text-white'>
+                                    <label className="btn btn-sm  bg-red-400 hover:bg-red-500 border-none">Delete</label>
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/pages/Dashboard/Admin/Allsellers.js
+++ src/pages/Dashboard/Admin/Allsellers.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import Loading from '../../../Loader/Loading';
 
 const Allsellers = () => {
+
+    const { data: sellers = [], isLoading } = useQuery({
+        queryKey: ['sellers',],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/users?role=seller`)
+            const data = await res.json();
+            return data;
+        }
+    })
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
         <div>
-            <p>All sellers</p>
+            <div className="overflow-x-auto ">
+                <table className="table w-full ">
+                    <thead className='bg-banner'>
+                        <tr>
+                            <th className='bg-nav-color text-white'>No</th>
+                            <th className='bg-nav-color text-white'>Name</th>
+                            <th className='bg-nav-color text-white'>Email</th>
+                            <th className='bg-nav-color text-white'>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody className='bg-banner'>
+                        {
+                            sellers.map((seller, i) => <tr key={seller._id}>
+                                <th className='bg-category text-white'>{i + 1}</th>
+                                <td className='bg-category text-white'>{seller?.name}</td>
+                                <td className='bg-category text-white'>{seller?.email}</td>
+                                <td className='bg-category text-white'>
+                                    <label className="btn btn-sm  bg-red-400 hover:bg-red-500 border-none">Delete</label>
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
